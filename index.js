2aa31f0c44adbea9ff10138a5dfe27aaa5d5bd26
Saqibaqeel <134763772+Saqibaqeel@users.noreply.github.com>
2023-07-26 20:03:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// console.log("well come to calculator")
+// let string=" ";
+// let button=document.querySelectorAll('.button');
+// Array.from(button).forEach((button)=>{
+   
+//     button.addEventListener('click',(e)=>{
+//         if(e.target.innerHTML=='='){
+//             string=eval(string)
+//             document.querySelector('input').value = string
+//         }
+//         else{
+//         console.log(e.target)
+//         string=string + e.target.innerHTML;
+       
+//         document.querySelector('input').value = string
+//         }
+        
+//     })
+// })
+
+
+let string = "";
+let buttons = document.querySelectorAll('button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
